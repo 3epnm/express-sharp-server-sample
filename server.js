68a83a487ad2bd29d16d67057c5c200d5bfcabe8
Marcel Bretschneider <info@3epnm.de>
2019-01-05 01:57:05 +0100

    Update Readme

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 const path = require('path');
 const express = require('express');
-const passport = require('passport');
-const Strategy = require('passport-http-bearer').Strategy;
+// const passport = require('passport');
+// const Strategy = require('passport-http-bearer').Strategy;
 const ImgSrv = require('express-sharp-server');
 
-const User = require('./User');
+// const User = require('./User');
 
-passport.use(new Strategy(
-  function (token, done) {
-    if (token === User.token) {
-      return done(null, User, { scope: 'all' });
-    } else {
-      return done(null, false);
-    } 
-  }));
+// passport.use(new Strategy(
+//   function (token, done) {
+//     if (token === User.token) {
+//       return done(null, User, { scope: 'all' });
+//     } else {
+//       return done(null, false);
+//     } 
+//   }));
 
-const { winston, morgan } = require('./logger.js');
+// const { winston, morgan } = require('./logger.js');
 
 const options = {
-  base_route: '/',
-  base_url: '/',
-  upload_dir: path.join(__dirname, 'data', 'uploads') + path.sep,
-  cache_dir: path.join(__dirname, 'data', 'cache') + path.sep,
+  // base_route: '/',
+  // base_url: '/',
+  // upload_dir: path.join(__dirname, 'data', 'uploads') + path.sep,
+  // cache_dir: path.join(__dirname, 'data', 'cache') + path.sep,
   // logger: winston
+
+  data_dir: path.join(__dirname, 'data')
 }
 
 const app = express();
 
-app.post(options.base_route,
-  passport.authenticate('bearer', { session: false }),
-  function(req, res, next) { next(); });
+// app.post(options.base_route,
+//   passport.authenticate('bearer', { session: false }),
+//   function(req, res, next) { next(); });
 
-app.put(options.base_route + ':guid',
-  passport.authenticate('bearer', { session: false }),
-  function(req, res, next) { next(); });
+// app.put(options.base_route + ':guid',
+//   passport.authenticate('bearer', { session: false }),
+//   function(req, res, next) { next(); });
 
-app.use(morgan);
+// app.use(morgan);
 app.use(ImgSrv(options));
 
 app.listen(61235);
